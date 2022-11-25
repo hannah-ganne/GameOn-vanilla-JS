39cb90f72190ef02fbee9f4c32e3e8cc65d16c13
lanomade <ganne.hannah@gmail.com>
2022-11-25 14:37:44 +0100

    add dom elements

--- starterOnly/form.js
+++ starterOnly/form.js
@@ -1,8 +1,16 @@
 // DOM elements
-
+const form = document.querySelector('form[name="reserve"]');
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const birthDate = document.getAnimations('birthdate');
+const quantity = document.getElementById('quantity');
+const location = document.querySelector('input[name="location"]:checked');
+const checkbox1 = document.getElementById('checkbox1');
+const checkbox2 = document.getElementById('checkbox2');
 
 // check minimum length requirement
-function checkMinimumLength(input, min, message) {
+function testMinimumLength(input, min, message) {
     if (input.length === 0) {
         showError(input, message)
     }
@@ -13,49 +21,57 @@ function checkMinimumLength(input, min, message) {
 }
 
 // check email address
-function checkEmail(input, message) {
+function testEmail(input, message) {
     const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
     regex.test(input.value)
-        ? showSuccess(input)
+        ? hideError(input)
         : showError(input, message);
 }
 
 // check if the value is numeric
-function checkNumber(input, message) {
-    isNaN(input)
+function testNumber(input, message) {
+    isNaN(input.value)
         ? showError(input, message)
-        : showSuccess(input)
+        : hideError(input)
 }
 
 // check if a radio button is checked
-function checkRadioBtn(name, input, message) {
+function testRadioBtn(name, input, message) {
     let checked_button = document.querySelector(`input[name = ${name}]:checked`);
 
     checked_button != null
-        ? showSuccess(input)
+        ? hideError(input)
         : showError(input, message)
 }
 
 // check if the checkbox is checked
-function checkCheckbox(id, input, message) {
+function testCheckbox(id, input, message) {
     let checkbox = document.getElementById(id);
 
     checkbox != null
-        ? showSuccess(input)
+        ? hideError(input)
         : showError (input, message)
 }
 
 // remove error message
-function showSuccess(input) {
-    const parent = input.parentElement;
-    const errorMsg = parent.lastElementChild;
-    errorMsg.textContent = ""
+function hideError(input) {
+    input.parentNode.removeAttribute('data-error');
+    input.parentNode.setAttribute('data-error-visible', false);
 }
 
 // show error message
 function showError(input, message) {
-    const parent = input.parentElement;
-    const errorMsg = parent.lastElementChild;
-    errorMsg.textContent = message;
+    input.parentNode.setAttribute('data-error', message);
+    input.parentNode.setAttribute('data-error-visible', true);
+}
+
+// validate form
+function validate() {
+    testMinimumLength(firstName, 2, "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    testMinimumLength(lastName, 2, "Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+    testEmail(email, "L'adresse électronique n'est pas valide");
+    testNumber(quantity, "Une valeur numérique doit être saisie");
+    testRadioBtn('location', location, "Vous devez choisir une option.");
+    testCheckbox('checkbox1', checkbox1, "Vous devez vérifier que vous acceptez les termes et conditions.");
 }
